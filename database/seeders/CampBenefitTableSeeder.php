2d4f82a0a7317d195136fcee2e0fdacdaf03ae45
Naufal Rusman <mnaufalrusmann2@gmail.com>
2022-05-30 20:41:36 +0700

    seeder complate

---
+++ database/seeders/CampBenefitTableSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\CampBenefit;
+
+class CampBenefitTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $campBenefits = [
+            [
+                'camp_id' => 1,
+                'name' => 'Pro Techstack Kit'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Mac Pro 2022 & Display'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => '1-1 Mentoring Program'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Final Project Certificate'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Offline Course Videos'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Future Job Oppotinity'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Primium Design Kit'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Website Builder'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => '1-1 Mentoring Program'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Final Project Certificate'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Offline Course Videos'
+            ]
+        ];
+
+        CampBenefit::insert($campBenefits);
+    }
+}
